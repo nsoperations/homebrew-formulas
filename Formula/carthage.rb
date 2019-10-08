cc4e4415c1628b3bd57b991ab2b69b7d0119af49
Werner Altewischer <werner.altewischer@ing.com>
2019-10-08 12:26:06 +0200

    Updated carthage formula

--- Formula/carthage.rb
+++ Formula/carthage.rb
@@ -2,19 +2,19 @@ class Carthage < Formula
   desc "Decentralized dependency manager for Cocoa"
   homepage "https://github.com/nsoperations/Carthage"
   url "https://github.com/nsoperations/Carthage.git",
-      :tag      => "0.39.2+nsoperations",
-      :version  => "0.39.2",
-      :revision => "ee209a56132b1d2308863739d68f25dab4db0660",
+      :tag      => "0.39.3+nsoperations",
+      :version  => "0.39.3",
+      :revision => "fc4cb6595ec5a309c85c4b563ec75148881610bf",
       :shallow  => false
   head "https://github.com/nsoperations/Carthage.git", :shallow => false
 
   depends_on :xcode => ["10.2", :build]
 
-  bottle do
-    root_url "https://dl.bintray.com/nsoperations/bottles-formulas"
-    cellar :any_skip_relocation
-    sha256 "e61b2fb427ec3d0687db90702fe57889a57379380ba8a22fe00059339c921974" => :mojave
-  end
+  #bottle do
+  #  root_url "https://dl.bintray.com/nsoperations/bottles-formulas"
+  #  cellar :any_skip_relocation
+  #  sha256 "e61b2fb427ec3d0687db90702fe57889a57379380ba8a22fe00059339c921974" => :mojave
+  #end
 
   def install
     if MacOS::Xcode.version >= "10.2" && MacOS.full_version < "10.14.4" && MacOS.version >= "10.14"
