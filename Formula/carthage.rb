35a598c5cf7ee42a241077c10ed2d7d01eee31d7
Werner Altewischer <werner.altewischer@ing.com>
2019-10-08 12:30:51 +0200

    Updated carthage formula

--- Formula/carthage.rb
+++ Formula/carthage.rb
@@ -10,11 +10,11 @@ class Carthage < Formula
 
   depends_on :xcode => ["10.2", :build]
 
-  #bottle do
-  #  root_url "https://dl.bintray.com/nsoperations/bottles-formulas"
-  #  cellar :any_skip_relocation
-  #  sha256 "e61b2fb427ec3d0687db90702fe57889a57379380ba8a22fe00059339c921974" => :mojave
-  #end
+  bottle do
+    root_url "https://dl.bintray.com/nsoperations/bottles-formulas"
+    cellar :any_skip_relocation
+    sha256 "c16e20054661a01143b0db6aa1cddd0a52658b875ea3699117ff9e4b8773c306" => :mojave
+  end
 
   def install
     if MacOS::Xcode.version >= "10.2" && MacOS.full_version < "10.14.4" && MacOS.version >= "10.14"
